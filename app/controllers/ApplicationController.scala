4cc70709c088e55e67556a5813d8914f213b04f9
Arturo Montoya <montoya332@live.com>
2017-03-06 17:45:39 -0800

    Added: JSON Response

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -10,14 +10,4 @@ class ApplicationController @Inject() extends Controller {
 	def index = Action {
 		Ok(views.html.index("Index Page"))
 	}
-	def login = Action {
-		Ok(views.html.index("login Page"))
-	}
-	def authenticate = Action {
-		Ok(views.html.index("authenticate Page"))
-	}
-	def logout = Action {
-		Ok(views.html.index("logout Page"))
-	}
-
 }
